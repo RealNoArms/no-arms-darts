d597bcac2c782a7458708ac91aaf9d9805b04d51
No Arms <timkracht4@gmail.com>
2013-08-22 13:38:02 -0400

    Added Arduino Shift Out Test

---
+++ Arduino/Tests/ShiftOutTest1/ShiftOutTest1.ino
@@ -0,0 +1,42 @@
+//**************************************************************//
+//  Name    : shiftOutCode, Hello World                                
+//  Author  : Carlyn Maw,Tom Igoe, David A. Mellis 
+//  Date    : 25 Oct, 2006    
+//  Modified: 23 Mar 2010                                 
+//  Version : 2.0                                             
+//  Notes   : Code for using a 74HC595 Shift Register           //
+//          : to count from 0 to 255                           
+//                                    
+//          : Modified to count to 15                          
+//****************************************************************
+
+//Pin connected to latch pin (ST_CP) of 74HC595
+const int latchPin = 3;
+//Pin connected to clock pin (SH_CP) of 74HC595
+const int clockPin = 2;
+////Pin connected to Data in (DS) of 74HC595
+const int dataPin = 7;
+
+void setup() {
+  //set pins to output so you can control the shift register
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+}
+
+void loop() {
+  // count from 0 to 255 and display the number 
+  // on the LEDs
+  for (int numberToDisplay = 0; numberToDisplay < 16; numberToDisplay++) {
+    // take the latchPin low so 
+    // the LEDs don't change while you're sending in bits:
+    digitalWrite(latchPin, LOW);
+    // shift out the bits:
+    shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);  
+
+    //take the latch pin high so the LEDs will light up:
+    digitalWrite(latchPin, HIGH);
+    // pause before next value:
+    delay(500);
+  }
+}
